83d82035f075149b954e44b9aae4acfc64831971
room43-33 <israelisraely11@gmail.com>
2025-02-17 16:03:12 +0200

    added leaderBoards

--- app/src/main/java/com/example/ultimatetictactoe/MainActivity.java
+++ app/src/main/java/com/example/ultimatetictactoe/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageButton[][] gameButtons;
     private DatabaseHelper dbHelper;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
